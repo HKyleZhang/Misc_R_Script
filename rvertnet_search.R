a0b241b2136ed30b2321e1a242c22529d5ff8cd6
Hongkai Zhang <hongkaizhang92@gmail.com>
2019-07-17 21:20:55 +0800

    R script to search in VertNet

---
+++ rvertnet_search.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(rvertnet)
+
+name_ls <- read_csv("namelist.csv")
+
+sp_ls <- as.character(name_ls$species)
+genus_ls <- as.character(name_ls$genus)
+
+res <- map2(sp_ls, 
+            genus_ls, 
+            ~searchbyterm(specificepithet = .x, 
+                          genus = .y, 
+                          limit = 1000, 
+                          year = c('>1980','<2020')
+                          )$data
+            )
+
+res %>% 
+  reduce(bind_rows) %>% 
+  write_excel_csv(path = paste0('vertnet_result_', 
+                          format(Sys.Date()),
+                          '.csv'), 
+            na = '')
+
